7fd737c4c8fb7c77563d735f76408db2dd081712
Jhonatan <jhow_72@usp.br>
2023-09-12 02:15:22 -0300

    refactor: tentativa de melhoria na legibilidade do codigo - add de error handlers

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,31 +3,61 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.math.BigDecimal;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Calculator {
     private static Logger logger = LogManager.getLogger(Calculator.class);
 
-    public Operation calculate(Operation operation){
-        OperationStrategy strategy;
-
-        switch (operation.getOperator()){
-            case '+': strategy = new Addition(); break;
-            case '-': strategy = new Subtraction(); break;
-            case '*': strategy = new Multiplication(); break;
-            case '/': strategy = new Division(); break;
-            default: strategy= null; break;
-        }
+    public Operation calculateOperation(Operation operation) {
+        OperationStrategy strategy = defineStrategy(operation.getOperator());
 
         if (strategy != null) {
-            operation.setResult(
-                    strategy.execute(operation.getValA(), operation.getValB())
-            );
+            try {
+                operation.setResult(strategy.execute(operation.getValA(), operation.getValB()));
+            }
+            catch (ArithmeticException e) {
+                throw e;
+            }
+            catch (Exception e) {
+                logger.error("Unexpected error while trying to execute the operation - " + e.getMessage());
+                throw new RuntimeException("Error while executing operation", e);
+            }
         }
         else {
             logger.warn("couldn't determine the operation strategy - setting the result to 0");
             operation.setResult(BigDecimal.ZERO);
         }
-
         return operation;
+    }
+
+    private OperationStrategy defineStrategy(char operator) {
+        return switch (operator) {
+            case '+' -> new Addition();
+            case '-' -> new Subtraction();
+            case '*' -> new Multiplication();
+            case '/' -> new Division();
+            default -> null;
+        };
+    }
+
+    public void calculateOperationsQueue(Queue<Operation> operationsQueue) {
+        Stack<BigDecimal> resultsStack = new Stack<>();
+        Printer printer = new Printer();
+
+        printer.printRemainingOperations(operationsQueue);
+
+        while(!operationsQueue.isEmpty()){
+            Operation operation = operationsQueue.poll();
+            System.out.println("\n-> Current operation: "+operation);
+
+            this.calculateOperation(operation);
+            System.out.print("-> Result: "+operation+"\n");
+            printer.printRemainingOperations(operationsQueue);
+
+            resultsStack.add(operation.getResult());
+        }
+
+        printer.printStack(resultsStack);
     }
 }
--- src/main/java/operations/Division.java
+++ src/main/java/operations/Division.java
@@ -1,14 +1,24 @@
 package operations;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class Division implements OperationStrategy{
+    private static final Logger logger = LogManager.getLogger(Division.class);
+
     @Override
     public BigDecimal execute(BigInteger valA, BigInteger valB) {
         BigDecimal bigDecimalA = new BigDecimal(valA);
         BigDecimal bigDecimalB = new BigDecimal(valB);
 
-        return bigDecimalA.divide(bigDecimalB);
+        try {
+            return bigDecimalA.divide(bigDecimalB);
+        } catch (ArithmeticException e){
+            logger.error("Check your inputs for the division operation- " + e.getMessage());
+            throw new ArithmeticException();
+        }
     }
 }
